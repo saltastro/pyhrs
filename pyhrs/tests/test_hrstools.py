1b31f1b29a65ed4167cf2039fa8aba56eaf625df
Steven Crawford <crawfordsm@gmail.com>
2014-11-20 01:49:49 +0200

    initial commit of testing file

---
+++ pyhrs/tests/test_hrstools.py
@@ -0,0 +1,13 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# This module tests the hrstools
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import numpy as np
+from numpy.testing import assert_array_equal
+from astropy.tests.helper import pytest
+
+from ..hrsmodel import HRSModel
+from PySpectrograph import SpectrographError
+
+
