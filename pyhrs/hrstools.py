a674cf55a65b26ada93ca07836d13498c8450456
Steven Crawford <crawfordsm@gmail.com>
2014-11-26 03:32:26 +0200

    pep8 fixes for hrstools

--- pyhrs/hrstools.py
+++ pyhrs/hrstools.py
@@ -6,6 +6,7 @@ import numpy as np
 from astropy import stats
 from astropy import modeling as mod
 
+
 def background(b_arr, niter=3):
     """Determine the background for an array
 
@@ -13,26 +14,27 @@ def background(b_arr, niter=3):
     ----------
     b_arr: numpy.ndarray
         Array for the determination of the background
- 
+
     niter: int
-        Number of iterations for sigma clipping  
+        Number of iterations for sigma clipping
 
     Returns
-    ------- 
+    -------
     bkgrd: float
-        median background value after sigma clipping 
+        median background value after sigma clipping
 
     bkstd: float
         Estimated standard deviation based on the median
         absolute deviation
-          
+
     """
-    cl_arr = stats.sigma_clip(b_arr, iters=niter, cenfunc=np.ma.median, 
+    cl_arr = stats.sigma_clip(b_arr, iters=niter, cenfunc=np.ma.median,
                               varfunc=stats.median_absolute_deviation)
-    return np.ma.median(cl_arr), 1.48*stats.median_absolute_deviation(cl_arr)
+    return np.ma.median(cl_arr), 1.48 * stats.median_absolute_deviation(cl_arr)
+
 
 def fit_order(data, detect_kernal, xc, order=3, ratio=0.5):
-    """Given an array and an overlapping detect_kernal, 
+    """Given an array and an overlapping detect_kernal,
     determine two polynomials that would outline the top
     and bottom of the order
 
@@ -51,10 +53,10 @@ def fit_order(data, detect_kernal, xc, order=3, ratio=0.5):
 
     order: int
         Order to use for the polynomial fit.
- 
+
     ratio: float
-        Limit at which to determine an order.  It is the 
-        ratio of the flux in the pixle to the flux at the 
+        Limit at which to determine an order.  It is the
+        ratio of the flux in the pixle to the flux at the
         peak.
 
     Returns
@@ -66,27 +68,26 @@ def fit_order(data, detect_kernal, xc, order=3, ratio=0.5):
         A polynomial that outlines the top of the order
 
     """
- 
-    #first thing to do is determine shape of the order
+
+    # first thing to do is determine shape of the order
     sdata = data * detect_kernal
     m_init = mod.models.Polynomial1D(order)
-    y,x = np.indices(sdata.shape)
+    y, x = np.indices(sdata.shape)
     #g_fit = mod.fitting.LevMarLSQFitter()
     g_fit = mod.fitting.LinearLSQFitter()
-    mask = (sdata>0)
+    mask = (sdata > 0)
     m = g_fit(m_init, x[mask], y[mask])
-    
-    #now the second thing to do is to determine the top and bottom of the 
-    #order
+
+    # now the second thing to do is to determine the top and bottom of the
+    # order
     y = int(m(xc))
-    n_arr = data[:,xc]
-    y1 = y-np.where(n_arr[:y] < ratio*data[y,xc])[0].max()
-    y2 = np.where(n_arr[y:] < ratio*data[y,xc])[0].min()
+    n_arr = data[:, xc]
+    y1 = y - np.where(n_arr[:y] < ratio * data[y, xc])[0].max()
+    y2 = np.where(n_arr[y:] < ratio * data[y, xc])[0].min()
     y_u = m.copy()
     y_l = m.copy()
 
-    y_l.parameters[0] = y_l.parameters[0]-y1
-    y_u.parameters[0] = y_u.parameters[0]+y2
-    
-    return y_l, y_u
+    y_l.parameters[0] = y_l.parameters[0] - y1
+    y_u.parameters[0] = y_u.parameters[0] + y2
 
+    return y_l, y_u
