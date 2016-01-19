7afeb2f0c48a17a9a4e39f9897c580370c0ee152
Steven Crawford <crawfordsm@gmail.com>
2016-01-19 13:39:09 +0200

    added CR cleaning to process

--- scripts/process.py
+++ scripts/process.py
@@ -22,6 +22,14 @@ if __name__=='__main__':
         exit('Are you sure this is an HRS file?')
 
    if len(sys.argv) == 5:
+      from astroscrappy import detect_cosmics
+      crmask, cleanarr = detect_cosmics(ccd.data, inmask=None, sigclip=4.5, sigfrac=0.3,
+                   objlim=5.0, gain=1.0, readnoise=6.5,
+                   satlevel=65536.0, pssl=0.0, niter=4,
+                   sepmed=True, cleantype='meanmask', fsmode='median',
+                   psfmodel='gauss', psffwhm=2.5, psfsize=7,
+                   psfk=None, psfbeta=4.765, verbose=False) 
+      ccd.data = cleanarr
       order_frame = CCDData.read(sys.argv[3], unit=u.adu)
       flat_frame = CCDData.read(sys.argv[4])
       ccd=flatfield_science(ccd, flat_frame, order_frame, median_filter_size=None, interp=True)
